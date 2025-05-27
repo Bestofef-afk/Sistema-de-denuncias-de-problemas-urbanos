9dc578b81e2a61af0f580285b7d7f6daa35b3cf1
caiofrvx <caioalbuquerque.jcv@gmail.com>
2025-05-27 15:53:30 -0300

    Header feito, footer também

--- sistemadenuncia/src/app/page.js
+++ sistemadenuncia/src/app/page.js
@@ -1,8 +1,16 @@
 'use client';
 import { useState } from 'react';
+import Header from '@/components/Header/page';
+import Footer from '@/components/Footer/page';
 
 export default function Home() {
   return (
-    <></>
+    <>
+   <Header>
+
+   </Header>
+
+   <Footer></Footer>
+    </>
   )
 }
