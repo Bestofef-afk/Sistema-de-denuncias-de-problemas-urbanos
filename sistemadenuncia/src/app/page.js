d39778004d0135a4bc614dcaa0afaba41007c95f
gaherrera00 <gabriel.h.demarchi@gmail.com>
2025-06-02 13:41:19 -0300

    Update page.js

--- sistemadenuncia/src/app/page.js
+++ sistemadenuncia/src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       {/* Mostrar só 4 */}
       <div className="flex items-center mb-8 space-x-3">
-        <h1 className="text-3xl font-semibold text-[#11703B]">Denúncias Registradas</h1>
+        <h1 className="text-3xl font-semibold text-[#11703B]">Denúncias Recentes</h1>
         <a
           href="/noticias"
           className="text-[#11703B] font-semibold hover:underline border border-[#11703B] px-4 py-1 rounded transition-colors hover:bg-[#11703B] hover:text-white"
@@ -20,7 +20,7 @@ export default function Home() {
           Ver Mais
         </a>
       </div>
-      <div className="max-h-[940px] overflow-hidden">
+      <div className="sm:max-h-[860px] md:max-h-[890px] lg:max-h-[940px] overflow-hidden">
         <ListaDenuncias />
       </div>
     </>
