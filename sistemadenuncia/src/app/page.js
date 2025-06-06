1428e1b8666f8a1084dfc624d3f2551f7bb4c417
caiofrvx <caioalbuquerque.jcv@gmail.com>
2025-06-06 16:42:17 -0300

    commit

--- sistemadenuncia/src/app/page.js
+++ sistemadenuncia/src/app/page.js
@@ -7,22 +7,29 @@ import ListaDenuncias from './noticias/page';
 
 export default function Home() {
   return (
-    <>
-      <Funcionario />
+    <div className="font-sans bg-[#F7F6F2] text-[#1C1C1C]">
+    
 
-      {/* Mostrar só 4 */}
-      <div className="flex items-center mb-8 space-x-3">
-        <h1 className="text-3xl font-semibold text-[#11703B]">Denúncias Recentes</h1>
-        <a
-          href="/noticias"
-          className="text-[#11703B] font-semibold hover:underline border border-[#11703B] px-4 py-1 rounded transition-colors hover:bg-[#11703B] hover:text-white"
-        >
-          Ver Mais
-        </a>
-      </div>
-      <div className="sm:max-h-[860px] md:max-h-[890px] lg:max-h-[940px] overflow-hidden">
-        <ListaDenuncias />
-      </div>
-    </>
-  )
+      <main className="max-w-7xl mx-auto px-4 py-6">
+        <Funcionario />
+
+        {/* Mostrar só 4 */}
+        <div className="flex items-center mb-8 space-x-3">
+          <h1 className="text-3xl font-semibold text-[#11703B]">Denúncias Recentes</h1>
+          <a
+            href="/noticias"
+            className="text-[#11703B] font-semibold hover:underline border border-[#11703B] px-4 py-1 rounded transition-colors hover:bg-[#11703B] hover:text-white"
+          >
+            Ver Mais
+          </a>
+        </div>
+
+        <div className="sm:max-h-[860px] md:max-h-[890px] lg:max-h-[940px] overflow-hidden">
+          <ListaDenuncias />
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
